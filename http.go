a6b4d53d36d5e9818b3ae21a8b1fbef828adbf25
Ido Kanner <idok@linesip.com>
2019-09-26 17:38:14 +0300

    basic api for incoming dlr

--- http.go
+++ http.go
@@ -100,3 +100,15 @@ func (h HTTPHandler) DoHTTP(
 
 	return
 }
+
+// OnGettingSMS is an HTTP server handler when incoming SMS arrives.
+// If mux exists, it will use it for a server, otherwise it will
+// use http.HandleFunc.
+func (h HTTPHandler) OnGettingSMS(path string, mux *http.ServeMux, httpHandler http.HandlerFunc) {
+	if mux != nil {
+		mux.HandleFunc(path, httpHandler)
+		return
+	}
+
+	http.HandleFunc(path, httpHandler)
+}
