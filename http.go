3544e817c19645510142c3eb7f98b8b89e65cf18
Ido Kanner <idok@linesip.com>
2019-10-03 13:48:38 +0300

    working on implementing the interface of smshandler

--- http.go
+++ http.go
@@ -1,15 +1,8 @@
 package telzarsms
 
 import (
-	"bytes"
-	"context"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
-	"os"
-	"strings"
 )
 
 // HTTPHandler perform HTTP actions, and implement
@@ -20,85 +13,7 @@ type HTTPHandler struct {
 // DoHTTP sends an HTTP Request for sending an SMS
 func (h HTTPHandler) DoHTTP(
 	method, contentType, address string, fields url.Values, body []byte) (resp *http.Response, err error) {
-
-	var request *http.Request
-	var bodyReader *bytes.Reader
-
-	fullAddress := fmt.Sprintf("%s", address)
-
-	if body != nil {
-		bodyReader = bytes.NewReader(body)
-	}
-
-	switch method {
-	case http.MethodGet:
-		fullAddress = fmt.Sprintf("%s?%s", fullAddress, fields.Encode())
-		request, err = http.NewRequest(http.MethodGet, fullAddress, bodyReader)
-	case http.MethodPost:
-		request, err = http.NewRequest(http.MethodPost, fullAddress, bodyReader)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	if contentType != "" {
-		request.Header.Set("Content-Type", contentType)
-	}
-	request.Close = true
-
-	ctx, cancel := context.WithTimeout(request.Context(), h.Client.Timeout)
-	defer cancel()
-	defer h.Client.CloseIdleConnections()
-
-	resp, err = h.Client.Do(request.WithContext(ctx))
-
-	if err != nil {
-		if strings.Contains(os.Getenv("SMSHTTPDEBUG"), "dump=true") {
-			fmt.Printf("Error was given: %s", err)
-		}
-		return
-	}
-
-	if resp == nil {
-		err = fmt.Errorf("resp is nil")
-		if strings.Contains(os.Getenv("SMSHTTPDEBUG"), "dump=true") {
-			fmt.Println("resp is nil")
-		}
-		return
-	}
-
-	if strings.Contains(os.Getenv("SMSHTTPDEBUG"), "dump=true") {
-		dump, err := httputil.DumpRequestOut(request, true)
-		fmt.Printf(">>>> dump request: %s \nerr: %s\n", dump, err)
-
-		dump, err = httputil.DumpResponse(resp, true)
-		fmt.Printf(">>>> dump response: %s \nerr: %s\n", dump, err)
-	}
-
-	if resp.Body == nil {
-		err = fmt.Errorf("resp.body is nil")
-		return
-	}
-
-	var respBody []byte
-	respBody, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-
-	if resp.StatusCode == http.StatusOK {
-		var status XMLResponse
-		status, err = FromXMLResponse(respBody)
-		if err != nil {
-			return
-		}
-		if status.Status != ErrorOK {
-			err = ToError(status)
-		}
-	}
-
-	return
+	return smshandler.DoHTTP(h.Client, method, contentType, address, fields, body)
 }
 
 // OnGettingSMS is an HTTP server handler when incoming SMS arrives.
--- response_xml.go
+++ response_xml.go
@@ -19,3 +19,23 @@ func FromXMLResponse(buf []byte) (XMLResponse, error) {
 	err := xml.Unmarshal(buf, &response)
 	return response, err
 }
+
+// ToError converts XMLResponse to SMSError. If everything is ok, it will return
+// nil
+func (r XMLResponse) ToError() *SMSError {
+	if r.Status == ErrorOK {
+		return nil
+	}
+	result := SMSError{
+		Status:     r.Status,
+		Message:    r.Message,
+		ShipmentID: r.ShipmentID,
+	}
+
+	return &result
+}
+
+// IsOK return true if the return status is OK
+func (r XMLResponse) IsOK() bool {
+	return r.Status == ErrorOK
+}
